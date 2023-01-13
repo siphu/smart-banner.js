bfe69ab076fd3b00028cc5479982ec5575ae4319
Phu  Nguyen <siphu@users.noreply.github.com>
2023-01-12 22:36:46 -0600

    Create smart-banner.js

---
+++ src/smart-banner.js
@@ -0,0 +1,33 @@
+const SmartBanner = (props) => {
+    const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+    const isIOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+    const {view_element,ios,android} = props;
+
+    const fakeScript = s => window.URL.createObjectURL(new Blob([s.toString().match(/^\s*\s*\(\s*\)\s*=>\s*\{(([\s\S](?!\}$))*[\s\S])/)[1]],{type:'text/javascript'}));
+    const makeWorker = (url,wait=0) => {
+        let worker = new Worker(fakeScript(() => {
+            self.addEventListener('message', e => {
+                if(e.data.wait && e.data.wait > 0)
+                    setTimeout(()=>{ self.postMessage(e.data.url); }, e.data.wait);
+                else 
+                    self.postMessage(e.data.url);
+            }, false);
+        }));
+        worker.addEventListener('message', function(e) { window.location=e.data; }, false);
+        worker.postMessage({ wait: wait ?? 0, url: url }); 
+    }
+
+    if(view_element) {
+        view_element.onclick = e => {
+            e.preventDefault();
+            if(isAndroid && android) {
+                window.location = android.intent || android.store;
+            } else if(isIOS && ios) {
+                makeWorker(ios.intent);
+                makeWorker(ios.store,10);
+            } else {
+                console.error('[Smart Banner]','Not a mobile device');
+            }
+        }
+    }
+}
