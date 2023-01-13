45c605f4710170c1251bed8c01269f57f6c36563
Phu  Nguyen <siphu@users.noreply.github.com>
2023-01-12 23:27:54 -0600

    Create smart-banner.set-timeout.js

---
+++ src/smart-banner.set-timeout.js
@@ -0,0 +1,23 @@
+/**
+* This is the same as smart-banner.js except using setTimeout and Promise for threading.
+**/
+const SmartBanner = (props) => {
+    const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+    const isIOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+    const {view_element,ios,android} = props;
+
+    if(view_element) {
+        view_element.onclick = e => {
+            e.preventDefault();
+            if(isAndroid && android) {
+                window.location = android.intent || android.store;
+            } else if(isIOS && ios) {
+                const openApp = async () => {  setTimeout(()=>{ window.location = ios.intent;  },10); };
+                const openStore = async () => {  setTimeout(()=>{ window.location = ios.store; },20); };
+                Promise.all([openStore(), openApp()]);
+            } else {
+                console.error('[Smart Banner]','Not a mobile device');
+            }
+        }
+    }
+}
